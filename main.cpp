5b29b0934d3930a97d18e8992b14d63700b1275c
Bentelben <mr.bentelben@gmail.com>
2025-09-16 21:50:54 +0300

    Feature: backward sorting

--- main.cpp
+++ main.cpp
@@ -5,13 +5,37 @@
 #include <assert.h>
 #include <string.h>
 
+int compare(void const *a, void const *b);
+int compareBackwards(void const *a, void const *b);
+
+
 int compare(void const *a, void const *b) {
-    return strcmp(*(char const **)a, *(char const **)b);
+    return strcmp(*(char const *const *)a, *(char const *const *)b);
 }
 
-//int compare(void const *a, void const *b) {
-//    return 
-//}
+int compareBackwards(void const *a, void const *b) {
+    char const *const str1 = *(char const *const *)a;
+    char const *const str2 = *(char const *const *)b;
+
+    size_t const str1_len = strlen(str1);
+    size_t const str2_len = strlen(str2);
+
+    for (size_t i = 1; i <= str1_len && i <= str2_len; i++) {
+        if (str1[str1_len - i] < str2[str2_len - i])
+            return -1;
+   
+        else if (str1[str1_len - i] > str2[str2_len - i])
+            return 1;
+    }
+    
+    if (str1_len < str2_len)
+        return -1;
+
+    else if (str1_len > str2_len)
+        return 1;
+
+    return 0;
+}
 
 
 int main() {
@@ -23,7 +47,7 @@ int main() {
 
     char **const lines = GetLineArray(text, line_count);
 
-    qsort(lines, line_count, sizeof(*lines), compare);
+    qsort(lines, line_count, sizeof(*lines), compareBackwards);
 
 
     for (size_t i = 0; i < line_count; i++) {
